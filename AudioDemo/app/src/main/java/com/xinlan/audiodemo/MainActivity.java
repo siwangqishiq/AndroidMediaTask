ef45c4010723b533090385e4b12036fdd1408900
siwangqishiq <525647740@qq.com>
2017-08-15 22:16:39 +0800

    播放PCM文件

--- AudioDemo/app/src/main/java/com/xinlan/audiodemo/MainActivity.java
+++ AudioDemo/app/src/main/java/com/xinlan/audiodemo/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -19,6 +20,7 @@ import java.io.IOException;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private View mStartBtn;
     private View mEndBtn;
+    private View mPlayBtn;
 
     private AudioCapture mCapture;
 
@@ -34,14 +36,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mStartBtn = findViewById(R.id.start_record);
         mEndBtn = findViewById(R.id.end_record);
+        mPlayBtn = findViewById(R.id.play_audio);
 
         mStartBtn.setOnClickListener(this);
         mEndBtn.setOnClickListener(this);
+        mPlayBtn.setOnClickListener(this);
 
         mCapture = new AudioCapture();
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, RECORD_PERMISSON_RECORD);
+            requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO,Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    RECORD_PERMISSON_RECORD);
         }
     }
 
@@ -54,7 +59,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.end_record:
                 endRecord();
                 break;
+            case R.id.play_audio:
+                playAudio();
+                break;
         }//end switch
+    }
+
+    protected void playAudio(){
+        if(mAudioFile == null)
+            return;
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                doPlay();
+            }
+        }).start();
+    }
+
+    private void doPlay(){
+        try {
+            AudioPlayer player = new AudioPlayer();
+            player.startPlayer();
+            int bufferSize = 2 *player.getMinBufferSize();
+            FileInputStream ips = new FileInputStream(mAudioFile);
+            int len = 0;
+            byte[] buffer = new byte[bufferSize];
+            while((len = ips.read(buffer,0,bufferSize))!=-1){
+                //System.out.println("read file len = "+len);
+                player.play(buffer, 0 , len);
+            }//end while
+            player.stopPlayer();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     protected void startRecord() {
